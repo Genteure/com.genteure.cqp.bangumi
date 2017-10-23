8bdd4f425273de9bbda9166c347239733089f228
Genteure <genteure@gmail.com>
2017-10-23 17:46:04 +0800

    basics

---
+++ com.genteure.cqp.bangumi/Bangumi.cs
@@ -0,0 +1,20 @@
+﻿using FluentScheduler;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.genteure.cqp.bangumi
+{
+    public static class Bangumi
+    {
+        private static Registry registry;
+
+        static Bangumi()
+        {
+            registry = new Registry();
+
+        }
+    }
+}
---
+++ com.genteure.cqp.bangumi/CoolQApi.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.genteure.cqp.bangumi
+{
+    internal static class CoolQApi
+    {
+
+        private static class NativeMethods
+        {
+
+        }
+    }
+}
